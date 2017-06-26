10a346f277187ad07701cd06f783504d9c622cb0
pwaryszak <dorpatczyk@yahoo.com>
2017-06-25 19:11:48 -0500

    I have added Map 3 that shows the location of my study sites in relation to entire continent of Australia.

--- Topsoil_Sites_Maps.R
+++ Topsoil_Sites_Maps.R
@@ -89,3 +89,41 @@ MyStudySites3 <- MyStudySites2a +theme( axis.text.x=element_text(size=18), #defi
                       axis.title.y=element_text(size=20),
                       legend.position = "none")
 MyStudySites3
+
+
+#MAP 3 Continent-scale of my Study Site Location======
+#To be included in my poster fro ESA2017 in Portland, USA
+if (!require(grid)) library(grid) # load ggplot package if not done already 
+if (!require(maptools)) library(maptools) # load ggplot package if not done already 
+if (!require(ggplot2)) library(ggplot2) # load ggplot package if not done already 
+if (!require(ggmap)) library(ggmap) # load ggplot package if not done already 
+
+SiteMapURL<- "https://raw.githubusercontent.com/PWaryszak/Topsoil_Sites/master/sitemap.csv"
+sitemap <- read.csv(SiteMapURL)#reading-in straight from online file.
+str(sitemap)#72 obs. of  10 variables:
+
+MyStudySitesPin<- data.frame(CentralLon = mean(sitemap$lon),
+                             CentralLat = mean(sitemap$lat),
+                             AusCentreLat = -25.610111,
+                             AusCentreLon = 134.354806 )
+MyStudySitesPin# Computed the central location for all my 6 sites (12 clusters of plot each)
+#CentralLon CentralLat
+# 115.9184  -32.18693
+
+#maptype can be = "roadmap", "watercolor", "terrain", "satellite", "toner"
+#I chose "toner"
+
+glgmap<-get_map(location = c(lon = MyStudySitesPin[,"AusCentreLon"], lat = MyStudySitesPin[,"AusCentreLat"])
+                ,maptype = "terrain", zoom = 4)#getting our map as base we will put our pin on. 
+ContFrame <- ggmap(glgmap)#combine google map with ggplot using ggmap function
+ContFrame #base map
+ContFrame1<-ContFrame + geom_point(aes(x = CentralLon, y = CentralLat, color = "red"),
+                                         size=15, shape = 0,stroke = 2, data = MyStudySitesPin) #placing the pin on our map.Play with shapes from 0 to 25.
+ContFrame2<-ContFrame1 +  ylab("") + xlab("")
+ContFrame3 <- ContFrame2 +theme( axis.text.x=element_text(size=10), #define your own theme parts
+                                        axis.text.y=element_text(size=10),
+                                        axis.title.x=element_text(size=12),
+                                        axis.title.y=element_text(size=12),
+                                        legend.position = "none")
+ContFrame3
+ggsave(ContFrame3, file="Topsoil_Sites_ContinetalScaleMap2.png", width=8, height=4)
